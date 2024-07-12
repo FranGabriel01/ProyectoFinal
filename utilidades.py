e05f7ded6ccd5095c351a4c7b641db31226f5bfa
Lopez Franco <100965486+FranGabriel01@users.noreply.github.com>
2024-07-12 11:25:23 -0300

    Update utilidades.py

--- utilidades.py
+++ utilidades.py
@@ -2,7 +2,6 @@ import pygame
 
 pygame.mixer.init()
 
-# Intentar cargar el sonido y manejar excepciones
 try:
     golpe_sonido = pygame.mixer.Sound(r'Sonidos\RPGSFX - 64 hit 1.ogg')
 except pygame.error as e:
