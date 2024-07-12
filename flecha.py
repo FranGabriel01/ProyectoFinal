cfc02e106ffac8eb07e767e530addda447938995
Lopez Franco <100965486+FranGabriel01@users.noreply.github.com>
2024-07-12 11:14:17 -0300

    Update flecha.py

--- flecha.py
+++ flecha.py
@@ -2,7 +2,25 @@ import pygame
 from contantes import * 
 
 class Flecha:
+    '''
+    Clase para representar una flecha en el juego utilizando Pygame.
+
+    Attributes:
+        x (int): La coordenada x de la flecha.
+        y (int): La coordenada y de la flecha.
+        velocidad (int): La velocidad de movimiento de la flecha.
+        direccion (int): La direccion en la que se mueve la flecha (1 para derecha, -1 para izquierda).
+        img (pygame.Surface): La imagen de la flecha.
+    '''
     def __init__(self, x, y, direccion):
+        '''
+        Inicializa una nueva flecha.
+
+        Args:
+            x (int): La coordenada x inicial de la flecha.
+            y (int): La coordenada y inicial de la flecha.
+            direccion (int): La direccion de la flecha (1 para derecha, -1 para izquierda).
+        '''
         self.x = x
         self.y = y
         self.velocidad = 10 * direccion
@@ -10,10 +28,25 @@ class Flecha:
         self.img = pygame.image.load(r'Imagenes\arrow\arrow_.png')
 
     def move(self):
+        '''
+        Mueve la flecha en la dirección especificada por su velocidad.
+        '''
         self.x += self.velocidad
 
     def draw_flecha(self, pantalla):
+        '''
+        Dibuja la flecha en la pantalla.
+
+        Args:
+            pantalla (pygame.Surface): La superficie de la ventana del juego.
+        '''
         pantalla.blit(self.img, (self.x, self.y))
 
     def flecha_fuera(self):
-        return self.x < 0 or self.x > 800
+        '''
+        Verifica si la flecha ha salido de la pantalla.
+
+        Returns:
+            bool: True si la flecha está fuera de los límites de la pantalla, False en caso contrario.
+        '''
+        return self.x < 0 or self.x > ANCHO
